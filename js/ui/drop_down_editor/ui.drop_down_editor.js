ab43ded8063426d8b308b5f187a105881c05eb90
marker dao ® | Roman Ty <mark_nikolsky@outlook.com>
2023-03-25 11:18:41 +0700

    fix(dateBox): Refactor && Add conditions

--- js/ui/drop_down_editor/ui.drop_down_editor.js
+++ js/ui/drop_down_editor/ui.drop_down_editor.js
@@ -101,8 +101,12 @@ const DropDownEditor = TextBox.inherit({
         });
     },
 
-    _shouldBeClosed() {
+    _isInstantlyMode() {
         return this.option('applyValueMode') === 'instantly';
+    },
+
+    _shouldBeClosed() {
+        return this._isInstantlyMode();
     },
 
     _getDefaultButtons: function() {
